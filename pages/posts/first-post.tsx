35996316a6c8a3dcc0dce3b0e76bad89c37d93f3
Christopher Shoo <rishaelibenson@gmail.com>
2020-10-09 23:41:07 +0300

    metadata for first post, Title of the page

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function FirstPost() {
   return (
     <div>
+      <Head>
+        <title>Fist post</title>
+      </Head>
       <h1>First post</h1>
       <Link href="/">
         <a>Go back to home page</a>
